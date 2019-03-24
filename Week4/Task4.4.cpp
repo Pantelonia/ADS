e144e5417235d9b343ccab0a1450dc180fe122c3
Paul <pavel_syrovatsky@mail.ru>
2019-03-24 23:29:31 +0300

    eight commit

--- Week4/Task4.4.cpp
+++ Week4/Task4.4.cpp
@@ -6,35 +6,34 @@ int main() {
 	ifstream input("input.txt");
 	ofstream output("output.txt");
 	char command;
-	long N, min, top, head, tail;
-	N = top = head = tail = 0;
-	top = -1;
+	long N, min, head, min_head, tail, min_tail;
+	N = head = min_head = 0;
+	min_tail = tail = -1;
 	input >> N;
 	long* queue = new long[N];
-	long* stack_of_min = new long[N];
+	long* queue_of_min = new long[N];
 	for (long i = 0; i < N; i++)
 	{
 		input >> command;
 		switch (command)
 		{
 		case '+':
-			input >> queue[tail];
-			stack_of_min[++top] = queue[tail];
-			min = top;
-			while (min > 0 && stack_of_min[min - 1] < stack_of_min[min])
-			{
-				swap(stack_of_min[min - 1], stack_of_min[min]);
-				min--;
+			input >> queue[++tail];
+			while (min_head - min_tail >= 0 && queue[tail] < queue_of_min[min_tail]) {
+				min_tail--;
 			}
-			tail++;
+
+			queue_of_min[++min_tail] = queue[tail];
+
 			continue;
 		case '-':
-			if (queue[head] == stack_of_min[top])
-				top--;
+			if (queue[head] == queue_of_min[min_head]) {
+				min_head++;
+			}
 			head++;
 			continue;
 		case'?':
-			output << stack_of_min[top] << '\n';
+			output << queue_of_min[min_head] << '\n';
 			continue;
 
 		}
